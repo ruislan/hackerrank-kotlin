c7c45ef712b5fdcc6c99d5076b819a0e3c7d7b0a
ruislan <ruislan@github.com>
2020-11-05 15:21:23 +0800

    solved SalesByMatch

---
+++ src/main/kotlin/practice/algorithms/implementation/SalesByMatch.kt
@@ -0,0 +1,24 @@
+package practice.algorithms.implementation
+
+import java.util.*
+
+// Complete the sockMerchant function below.
+fun sockMerchant(n: Int, ar: Array<Int>): Int {
+    // 方法1
+    // 利用hashmap存储同种颜色的🧦的数量
+    // 迭代hashmap，将每个数量 / 2 并求和
+    // 这个和就是最终结果
+    return ar.groupingBy { it }.eachCount().map { it.value / 2 }.sum()
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    val ar = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
+
+    val result = sockMerchant(n, ar)
+
+    println(result)
+}
