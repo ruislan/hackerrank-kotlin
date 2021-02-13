284a3de75f53e80ef636df93db5c980ef6a42960
ruislan <ruislan@github.com>
2021-02-14 00:58:31 +0800

    solved "Common Child"

--- src/main/kotlin/practice/algorithms/strings/medium/Common_Child.kt
+++ src/main/kotlin/practice/algorithms/strings/medium/Common_Child.kt
@@ -1,14 +1,21 @@
 package practice.algorithms.strings.medium
 
+import java.util.*
+
+// https://www.hackerrank.com/challenges/common-child/problem
+
 // 方法1
 // 最长公共子序列
 // 经典DP解决
+// AC
 fun main(args: Array<String>) {
-    val s1 = readLine()!!
-    val s2 = readLine()!!
+    val scan = Scanner(System.`in`)
+    val s1 = scan.nextLine()
+    val s2 = scan.nextLine()
+
     val n = s1.length
     val m = s2.length
-    val dp = Array(n + 1) { Array(m + 1) { 0 } }
+    val dp = Array(n + 1) { IntArray(m + 1) }
     for (i in 1..n) {
         for (j in 1..m) {
             if (s1[i - 1] == s2[j - 1]) {
@@ -18,5 +25,6 @@ fun main(args: Array<String>) {
             }
         }
     }
+
     println(dp[n][m])
 }
